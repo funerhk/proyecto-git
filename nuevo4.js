0ee55f4f2711c92514e275024d34ca7bb4829ef4
daniel corona <funerhk@gmail.com>
2022-07-18 22:29:08 -0500

    version 1.0

---
+++ nuevo4.js
@@ -0,0 +1,25 @@
+instrucciones paara git 
+
+intruccion para iniciar un proyecto como git
+git init
+
+instruccion para agregar archivos
+git add "nombre del archivo sin las comillas"
+
+para agregar todo lo pendeinte
+git add.
+
+configuracion global
+git config --global user.email "funerhk@gmail.com"
+git config --global user.name "daniel corona"
+
+para hacer commit 
+git commit -m "version 1.0"
+
+para consultar el estado del repositorio asi como las cambios y commits pendientes
+git status
+
+
+
+
+
